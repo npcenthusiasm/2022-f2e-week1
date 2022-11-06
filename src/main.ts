7b0157c5b6f8d86afe09158f0ea1c57f356f73f3
npcenthusiasm <npcenthusiasm@gmail.com>
2022-11-06 22:12:04 +0800

    add base style & sass

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+
+// tailwind
 import './style.css'
+// global css
+import './assets/scss/all.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#007FAB',
+          dark: '#003A4F',
+          tint: '#B0D2DE'
+        },
+        secondary: {
+          DEFAULT: '#FFC37D',
+          dark: '#A46039',
+          tint: '#FFE2A9',
+        },
+        black: {
+          DEFAULT: '#000000',
+          800: '#CCCCCC',
+          600: '#999999',
+          400: '#666666',
+          200: '#333333',
+          0: '#FFFFFF'
+        },
+        highlight: {
+          DEFAULT: '#FF5136',
+          dark: '#CD331A',
+          tint: '#FFB5A4',
+        },
+      }
+    },
   },
   plugins: [require("@tailwindcss/line-clamp")],
 };
