6dbf9c448f2bc402c779d1b80e01344f066361f6
npcenthusiasm <npcenthusiasm@gmail.com>
2022-11-07 00:43:23 +0800

    add index & update images

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,239 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<template>
+  <div class="bg-secondary h-screen relative">
+    <img src="/images/logo/logo_text.png" alt="" class="logo-text">
+    <img src="/images/main/start.png" alt="" class="start-bg">
+    <img src="/images/main/road.png" alt="" class="road-bg">
+    <img src="/images/bg/bg_decorate_01.png" alt="" class="cloud-1">
+    <img src="/images/bg/bg_decorate_05.png" alt="" class="cloud-2">
+
+    <img src="/images/character/character_f2e.gif" alt="" class="ch_f2e">
+    <img src="/images/character/character_ui.gif" alt="" class="ch_ui">
+    <img src="/images/character/character_team.gif" alt="" class="ch_team">
+
+
+    <div class="btn-join-area">
+      <div class="text-primary text-title-h5 mb-2">JOIN</div>
+      <img src="/images/btn/btn_joinHand.png" alt="" class="btn_joinHand">
+      <img src="/images/btn/btn_join.png" alt="" class="btn_join">
+    </div>
+
+
+    <img src="/images/main/map.svg" alt="" class="map">
+
+
+
+    <div class="light-area inline-block">
+      <div class="text-center text-highlight text-title-h4">READY?</div>
+      <div class="light-wrap">
+        <img src="/images/main/ready_frame.png" alt="" class="ready_frame">
+        <img src="/images/main/ready_1.png" alt="" class="ready_1">
+        <img src="/images/main/ready_2.png" alt="" class="ready_2">
+        <img src="/images/main/ready_3.png" alt="" class="ready_3">
+      </div>
+    </div>
+
+
+    <div class="title-text bg-highlight text-white text-title-h4 inline-block px-10 py-2 rounded-full">互動式網頁設計</div>
+
+    <div class="f2e-info-area">
+      <div class="f2e-area">
+        <div class="text-primary text-title-h5 mb-2">前端工程師</div>
+        <div class="bg-primary text-white text-title-h4 inline-block px-10 py-1 rounded-full">920</div>
+      </div>
+
+      <div class="f2e-area">
+        <div class="text-primary text-title-h5 mb-2">UI 設計師</div>
+        <div class="bg-primary text-white text-title-h4 inline-block px-10 py-1 rounded-full">920</div>
+      </div>
+
+      <div class="f2e-area">
+        <div class="text-primary text-title-h5 mb-2">團體組</div>
+        <div class="bg-primary text-white text-title-h4 inline-block px-10 py-1 rounded-full">41</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import gsap from 'gsap'
+
 import { onMounted } from "vue";
 
 onMounted(() => {
-  const script = document.createElement("script");
+    // gsap.to('.logo-text',  {
+    //   x: '50%',
+    //   y: '50%'
+    // })
+  // gsap.
+  // const script = document.createElement("script");
 
-  script.src = "https://buttons.github.io/buttons.js";
-  script.async = true;
+  // script.src = "https://buttons.github.io/buttons.js";
+  // script.async = true;
 
-  document.body.appendChild(script);
+  // document.body.appendChild(script);
 });
 </script>
 
-<template>
-  <div class="bg-gray-800 h-screen text-white">
-    <header class="py-16">
-      <h1 class="text-5xl font-bold text-center mb-6">Create Tailwind</h1>
-      <p className="text-center mb-6 text-xl">
-        If you like this project, consider giving it a star on GitHub!
-      </p>
+<style lang="scss" scoped>
+@use "sass:math";
+@function relative-height-percent($value) {
+  @return math.div($value, 1080px) * 100%;
+}
 
-      <div class="flex flex-row justify-center items-center gap-4">
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tailwind-app"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count="true"
-          aria-label="Star andrejjurkin/create-tailwind-app on GitHub"
-        >
-          Star
-        </a>
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tailwind-app/discussions"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-comment-discussion"
-          data-size="large"
-          aria-label="Discuss andrejjurkin/create-tailwind-app on GitHub"
-        >
-          Discuss
-        </a>
-      </div>
-    </header>
+@function relative-width-percent($value) {
+  @return math.div($value, 1920px) * 100%;
+}
 
-    <div class="flex gap-16 items-center justify-center">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="w-24 h-24" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="w-24 h-24" alt="Vue logo" />
-      </a>
-    </div>
-  </div>
-</template>
+.logo-text {
+  position: absolute;
+  top: relative-height-percent(44px);
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+  width: relative-width-percent(680px);
+}
+
+.start-bg {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  bottom: 0;
+  transform: translateX(-50%);
+  width: relative-width-percent(1430px);
+  height: 100%;
+  z-index: 5;
+}
+
+.road-bg {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+  width: relative-width-percent(1175px);
+}
+
+.cloud-1 {
+  position: absolute;
+  top: relative-height-percent(423px);
+  left: relative-width-percent(142px);
+  width: relative-width-percent(430px);
+}
+
+.cloud-2 {
+  position: absolute;
+  right: 0;
+  top: relative-height-percent(455px);
+  right: relative-width-percent(91px);
+  width: relative-width-percent(485px);
+}
+
+// gif
+
+.ch_f2e {
+  position: absolute;
+  bottom: relative-height-percent(16px);
+  left: relative-width-percent(449px);
+  width: relative-width-percent(351px);
+}
+
+.ch_ui {
+  position: absolute;
+  bottom: relative-height-percent(27.5px);
+  left: relative-width-percent(776px);
+  width: relative-width-percent(370px);
+
+}
+
+.ch_team {
+  position: absolute;
+  bottom: relative-height-percent(9px);
+  right: relative-width-percent(439px);
+  width: relative-width-percent(351px);
+}
+
+.btn-join-area {
+  position: absolute;
+  bottom: relative-height-percent(20px);
+  right: relative-width-percent(20px);
+  text-align: center;
+
+  .btn_joinHand {
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 57px;
+  }
+  .btn_join {
+    width: 103px;
+  }
+}
+
+.light-area {
+  position: absolute;
+  right: 0;
+
+  top: relative-height-percent(463px);
+  // TODO: 改 realtive
+}
+
+.f2e-info-area {
+  display: flex;
+  justify-content: space-around;
+  max-width: 1196px;
+  width: 100%;
+  position: absolute;
+  top: relative-height-percent(371px);
+  left: relative-width-percent(362px);
+}
+
+.light-wrap {
+  position: relative;
+  width: 275px;
+  
+  .ready_frame {
+    position: absolute;
+    z-index: 1;
+  }
+
+  .ready_1,
+  .ready_2,
+  .ready_3 {
+    position: absolute;
+    width: 49px;
+    top: 29px;
+
+  }
+
+  .ready_3 {
+    left: 32px;
+  }
+  .ready_2 {
+    left: 97px;
+  }
+  .ready_1 {
+    left: 162px;
+  }
+}
+
+.title-text {
+  position: absolute;
+  top: relative-height-percent(268px);
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.f2e-area {
+  text-align: center;
+}
+
+.map {
+  position: absolute;
+  bottom: relative-height-percent(20px);
+  left: relative-width-percent(30px);
+  width: 260px;
+}
+
+</style>
+
+
